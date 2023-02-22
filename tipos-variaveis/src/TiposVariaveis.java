ca4e48c88ff569fa13ddfbba789fdc7761cdd568
alexp1203 <alexpinheiro1203@gmail.com>
2023-02-21 21:54:55 -0300

    teste commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        //double salarioMinimo = 2500; //2.500 significa 2,5
+
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal;
+
+        //final double VALOR = 5.45; //final > constante, não permite alterar o valor
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
     }
 }
