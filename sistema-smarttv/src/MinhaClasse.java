bd087d2dc189b2efa66ddcb80627f55d9d4046db
alexp1203 <alexpinheiro1203@gmail.com>
2023-02-25 21:05:02 -0300

    teste de execução no prompt de comando e terminal

---
+++ sistema-smarttv/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+
+    public static void main(String[] args){
+        System.out.println("Olá Mundo sendo executado pelo terminal");
+
+        int num = 1;
+        int num2 = 2;
+        System.out.println(num + nun2);
+    }
+}
